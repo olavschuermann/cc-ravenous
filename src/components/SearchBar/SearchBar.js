66912672aad719c2247a206bd8507ebc7c3b23be
Olav Schuermann <developer@netzpluslogik.de>
2019-03-25 10:20:59 +0100

    project part 1 finished

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Searchbar.css';
+import './SearchBar.css';
 
 const sortByOptions = {
     'Best Match': 'best_match',
@@ -14,4 +14,24 @@ class SearchBar extends React.Component {
             return <li key={sortByOptionValue}> {sortByOption} </li>;
         });
       }
+      render() {
+          return (
+            <div className="SearchBar">
+            <div className="SearchBar-sort-options">
+              <ul>
+                {this.renderSortByOptions()}
+              </ul>
+            </div>
+            <div className="SearchBar-fields">
+              <input placeholder="Search Businesses" />
+              <input placeholder="Where?" />
+            </div>
+            <div className="SearchBar-submit">
+              <a>Let's Go</a>
+            </div>
+          </div>
+          )
+      }
 }
+
+export default SearchBar;
