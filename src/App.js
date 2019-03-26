d9e03e882f6fbba7a37d49d0db22e9ef6b8d2639
Olav Schuermann <developer@netzpluslogik.de>
2019-03-26 12:48:21 +0100

    project part 2 finished

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses = {businesses}/>
       </div>
     );
   }
